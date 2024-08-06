ae1615aef45faa5a915e30ebef9d398457939961
Alexis Bahiano Paoletti <bay-pao2011@hotmail.com>
2024-08-06 15:57:11 -0300

    //

--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -17,8 +17,8 @@ namespace CapaPresentacion
 			System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("es-AR");
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			//Application.Run(new Login());
-			Application.Run(new Inicio());
+			Application.Run(new Login());
+			//Application.Run(new Inicio());
 		}
 	}
 }
